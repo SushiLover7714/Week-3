6fe1040121b6b033c05cc889b1ca0d81535974fe
SushiLover7714 <138302226+SushiLover7714@users.noreply.github.com>
2023-10-03 22:05:03 +0630

    Update DealerShipMain.java

--- Week 3/5.9.23/DealerShipMain.java
+++ Week 3/5.9.23/DealerShipMain.java
@@ -125,20 +125,29 @@ public class DealerShipMain {
       }
 
       else if (option == 3) {
+        input.nextLine();
+        System.out.print("Enter maximum price: ");
+        int maxPrice = input.nextInt();
+        System.out.print("Enter minimum price: ");
+        int minPrice = input.nextInt();
+        for (int i = 0; i < cars.size(); i++) {
+          if (cars.get(i).getPrice() < maxPrice && cars.get(i).getPrice() > minPrice) {
+            cars.get(i).showCarInformation();
+          }
+        }
 
-      }
-
-    } else if (option == 2) {
-      input.nextLine();
-      System.out.print("Enter Customer ID : ");
-      String customerID = input.nextLine();
-      int index = getCustomerIndexByID(customerID, customers);
-      if (index != -1) {
-        System.out.println(
-            "------------Customer " + customerID + "'s cars------------");
-        customers.get(index).showCustomerCarInformation();
-      } else {
-        System.out.println("\nInvalid Customer ID");
+      } else if (option == 2) {
+        input.nextLine();
+        System.out.print("Enter Customer ID : ");
+        String customerID = input.nextLine();
+        int index = getCustomerIndexByID(customerID, customers);
+        if (index != -1) {
+          System.out.println(
+              "------------Customer " + customerID + "'s cars------------");
+          customers.get(index).showCustomerCarInformation();
+        } else {
+          System.out.println("\nInvalid Customer ID");
+        }
       }
     }
   }
